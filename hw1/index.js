328e0903ef64f768bd94b8961b4f00b3b449a69c
SvetlanaNaruleva <swvolkova49@gmail.com>
2023-12-20 00:17:11 +0300

    домашнее задание 2.js

---
+++ hw1/index.js
@@ -0,0 +1,88 @@
+// Задание 1
+
+let a = 10;
+a = 20;
+alert (a);
+
+// Задание 2
+
+let year = String(alert(`Год выпуска первого айфона 2007 год`));
+
+// Задание 3
+
+let nameJs = (alert(`Имя создателя языка JavaScript: Brendan Eich`));
+
+// Задание 4
+
+let ten = 10;
+let two = 2;
+alert (ten + two);
+alert (ten * two);
+alert (ten / two);
+alert (ten - two);
+
+// Задание 5
+let b = 2
+let c = 5
+let result = (b ** c)
+alert(result);
+
+
+// Задание 6
+
+let one = 9
+let twoTwo = 2
+let e = one % twoTwo
+alert (e);
+
+// Задание 7
+
+let num = 1;
+num = num += 5;
+num = num -= 3;
+num = num *= 7;
+num = num /= 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+// Задание 8
+
+let age = Number (prompt("Сколько вам лет?"));
+alert (age);
+
+// Задание 9.0
+
+const user = {
+    name:`Bob`,
+    age: Number ("14"),
+    isAdmin: Boolean = true,
+};
+alert(user.name)
+alert(user.age)
+
+// Задание 9.1
+
+user["city of residence"] = `Krasnodar`;
+alert(user["city of residence"]);
+
+// Задание 9.2
+
+user.age = 14;
+
+alert(user.age + 14);
+
+// Задание 9.3
+
+delete user["city of residence"]
+
+//Задание 9.4
+
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info])
+
+// Задание 10
+
+let yourName
+yourName = prompt(`Как Вас зовут?`);
+alert(`Привет, ${yourName}!`)
